065b593a6d7392cdc65d3a22f978359d06cfc40d
mejdi <mejdi@ounitech.net>
2024-04-11 15:56:19 +0200

    rename to d5 for consistency

--- src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -4,14 +4,14 @@ public class Yatzy1 {
 
     private int[] dice;
 
-    public Yatzy1(int d1, int d2, int d3, int d4, int _5)
+    public Yatzy1(int d1, int d2, int d3, int d4, int d5)
     {
         dice = new int[5];
         dice[0] = d1;
         dice[1] = d2;
         dice[2] = d3;
         dice[3] = d4;
-        dice[4] = _5;
+        dice[4] = d5;
     }
 
     public static int chance(int d1, int d2, int d3, int d4, int d5)
