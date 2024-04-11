522de00e78a37ea88239dab10b86ee1399ea4286
mejdi <mejdi@ounitech.net>
2024-04-11 16:17:06 +0200

    refactor yatzy to match other methods

--- src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -25,9 +25,10 @@ public class Yatzy1 {
         return total;
     }
 
-    public static int yatzy(int... dice)
+    public static int yatzy(int d1, int d2, int d3, int d4, int d5)
     {
         int[] counts = new int[6];
+        int[] dice= {d1, d2, d3, d4, d5};
         for (int die : dice)
             counts[die-1]++;
         for (int i = 0; i != 6; i++)
