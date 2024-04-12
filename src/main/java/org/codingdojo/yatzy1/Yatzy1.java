1a255a26db973603c2932e549f24633ba7ef20ea
mejdi <mejdi@ounitech.net>
2024-04-12 10:26:36 +0200

    make method name more explicit and add an example

--- src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -9,7 +9,7 @@ public class Yatzy1 {
 
     public int yatzy(DiceRoll diceRoll)
     {
-        int[] counts = countOccurrences(diceRoll);
+        int[] counts = countOccurrencesOfSide(diceRoll);
         for (int i = 0; i < 6; i++){
             if (counts[i] == 5){
                 return 50;
@@ -47,7 +47,7 @@ public class Yatzy1 {
 
     public int onePair(DiceRoll diceRoll)
     {
-        int[] counts = countOccurrences(diceRoll);
+        int[] counts = countOccurrencesOfSide(diceRoll);
         for (int i = 0; i < 6; i++){
             if (counts[6-i-1] >= 2) { // reverse order to capture the highest pair first
                 return (6-i)*2;
@@ -58,7 +58,7 @@ public class Yatzy1 {
 
     public int twoPairs(DiceRoll diceRoll)
     {
-        int[] counts = countOccurrences(diceRoll);
+        int[] counts = countOccurrencesOfSide(diceRoll);
         int numberOfPairs = 0;
         int score = 0;
         for (int i = 0; i < 6; i ++){
@@ -72,7 +72,7 @@ public class Yatzy1 {
 
     public int fourOfAKind(DiceRoll diceRoll)
     {
-        int[] counts = countOccurrences(diceRoll);
+        int[] counts = countOccurrencesOfSide(diceRoll);
         for (int i = 0; i < 6; i++){
             if (counts[i] >= 4) {
                 return (i+1) * 4;
@@ -83,7 +83,7 @@ public class Yatzy1 {
 
     public int threeOfAKind(DiceRoll diceRoll)
     {
-        int[] counts = countOccurrences(diceRoll);
+        int[] counts = countOccurrencesOfSide(diceRoll);
         for (int i = 0; i < 6; i++){
             if (counts[i] >= 3) {
                 return (i+1) * 3;
@@ -94,7 +94,7 @@ public class Yatzy1 {
 
     public int smallStraight(DiceRoll diceRoll)
     {
-        int[] counts = countOccurrences(diceRoll);
+        int[] counts = countOccurrencesOfSide(diceRoll);
         if (counts[0] == 1 &&
             counts[1] == 1 &&
             counts[2] == 1 &&
@@ -106,7 +106,7 @@ public class Yatzy1 {
 
     public int largeStraight(DiceRoll diceRoll)
     {
-        int[] counts = countOccurrences(diceRoll);
+        int[] counts = countOccurrencesOfSide(diceRoll);
         if (counts[1] == 1 &&
             counts[2] == 1 &&
             counts[3] == 1 &&
@@ -118,7 +118,7 @@ public class Yatzy1 {
 
     public int fullHouse(DiceRoll diceRoll)
     {
-        int[] counts = countOccurrences(diceRoll);
+        int[] counts = countOccurrencesOfSide(diceRoll);
 
         boolean isThereAPair = false;
         int pairIndex = 0;
@@ -144,14 +144,22 @@ public class Yatzy1 {
 
 
     /**
-     * calculate the occurrence of a number in a DiceRoll
+     * calculate the occurrence of a side (or number) in a DiceRoll
      * and returns an array with these occurrences at index-1
      *  The occurrence of ones is in index 0
      *  The occurrence of sixes is in index 5
+     *  Example :
+     *  countOccurrencesOfSide(new DiceRoll(1,3,6,3,1)) return array {2, 0, 2, 0, 0, 1}
+     *      occurrence of side 1 is 2 and is placed at index 0
+     *      occurrence of side 2 is 1 and is placed at index 1
+     *      occurrence of side 3 is 2 and is placed at index 2
+     *      occurrence of side 4 is 0 and is placed at index 3
+     *      occurrence of side 5 is 0 and is placed at index 4
+     *      occurrence of side 6 is 1 and is placed at index 5
      * @param diceRoll
      * @return array of size 5
      */
-    private static int[] countOccurrences(DiceRoll diceRoll) {
+    private static int[] countOccurrencesOfSide(DiceRoll diceRoll) {
         int[] counts = new int[6];
         counts[diceRoll.getD1() -1]++;
         counts[diceRoll.getD2() -1]++;
